081b325202ea77adc3abf1c3618a49d711829743
Alex Page <alex@alexpage.com.au>
2019-03-23 22:05:59 -0400

    Add some better logs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,12 @@
 const GitHubPages = require( 'gh-pages' );
 
+const siteDirectory = 'site';
+const deployBranch = 'gh-pages';
+
 GitHubPages.publish(
-	'site',
+	siteDirectory,
 	{
+		branch: deployBranch,
 		repo: `https://${ process.env.GITHUB_TOKEN }@github.com/alex-page/alex-page.git`,
 		user: {
 			name: 'Alex Page',
@@ -13,8 +17,10 @@ GitHubPages.publish(
 	},
 	( error ) => {
 		if( error ) {
-			throw new Error( `GitHub pages deploy failed: ${ error }` );
+			throw new Error( `❌ GitHub pages deployment failed: ${ siteDirectory } directory to ${ deployBranch } branch\n${ error }` );
 		}
-		console.log( 'GitHub pages successfully deployed' );
+		console.log( `✅ GitHub pages deployment successfull: ${ siteDirectory } directory to ${ deployBranch } branch` );
 	}
 );
+
+// https://twitter.com/aalexpaage/status/1109619279027015680
