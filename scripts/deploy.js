ebf1fb6cd329669faa89dd602b17ad0972c21b65
Alex Page <alex@alexpage.com.au>
2019-08-10 11:13:46 -0400

    Remove async from deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,29 +4,31 @@ const GitLastCommit = new( require( "last-commit-log" ) )();
 
 
 // Deploy the site
-(async() => {
-	const commitMessage = ( await GitLastCommit.getLastCommit() ).subject;
-	const siteDirectory = "site";
-	const deployBranch = "gh-pages";
+GitLastCommit.getLastCommit()
+	.then(commit => {
+		const siteDirectory = "site";
+		const deployBranch = "gh-pages";
+		const commitMessage = commit.subject;
 
-	GitHubPages.publish(
-		siteDirectory,
-		{
-			branch: deployBranch,
-			repo: `https://${ process.env.GH_PAT }@github.com/alex-page/alex-page.git`,
-			user: {
-				name: "Alex Page",
-				email: "alex@alexpage.com.au",
+		GitHubPages.publish(
+			siteDirectory,
+			{
+				branch: deployBranch,
+				repo: `https://${ process.env.GH_PAT }@github.com/alex-page/alex-page.git`,
+				user: {
+					name: "Alex Page",
+					email: "alex@alexpage.com.au",
+				},
+				silent: true,
+				dotfiles: true,
+				message: commitMessage,
 			},
-			silent: true,
-			dotfiles: true,
-			message: commitMessage,
-		},
-		( error ) => {
-			if( error ) {
-				throw new Error( `❌ Failed to deploy to GitHub pages: ${ siteDirectory } directory failed to push to ${ deployBranch } branch\n${ error }` );
+			( error ) => {
+				if( error ) {
+					throw new Error( `❌ Failed to deploy to GitHub pages: ${ siteDirectory } directory failed to push to ${ deployBranch } branch\n${ error }` );
+				}
+				console.log( `✅ Successfully deployed to GitHub pages: ${ siteDirectory } directory pushed to ${ deployBranch } branch` );
 			}
-			console.log( `✅ Successfully deployed to GitHub pages: ${ siteDirectory } directory pushed to ${ deployBranch } branch` );
-		}
-	);
-})();
+		);
+	})
+	.catch( error => console.error(error))
