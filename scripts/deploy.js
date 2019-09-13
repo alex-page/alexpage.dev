c67cfe1f69e4a8ec374e3d458d74c1d9ed9ae167
Alex Page <alex@alexpage.com.au>
2019-09-13 19:05:15 -0400

    convert deploy to async

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,32 +2,34 @@
 const GitHubPages = require( "gh-pages" );
 const GitLastCommit = new( require( "last-commit-log" ) )();
 
+const siteDirectory = "_site";
+const deployBranch = "gh-pages";
 
 // Deploy the site
-GitLastCommit.getLastCommit()
-	.then(commit => {
-		const siteDirectory = "site";
-		const deployBranch = "gh-pages";
-		const commitMessage = commit.subject;
+(async() => {
+	try {
+		const {subject} =  await GitLastCommit.getLastCommit();
 
-		GitHubPages.publish(
+		await new Promise((resolve, reject) => GitHubPages.publish(
 			siteDirectory,
 			{
 				branch: deployBranch,
 				repo: `https://${ process.env.GH_PAT }@github.com/alex-page/alex-page.git`,
 				user: {
 					name: "Alex Page",
-					email: "alex@alexpage.com.au",
+					email: "alex.page@shopify.com",
 				},
 				dotfiles: true,
-				message: commitMessage,
+				message: subject,
 			},
-			( error ) => {
-				if( error ) {
-					throw new Error( `❌ Failed to deploy to GitHub pages: ${ siteDirectory } directory failed to push to ${ deployBranch } branch\n${ error }` );
-				}
-				console.log( `✅ Successfully deployed to GitHub pages: ${ siteDirectory } directory pushed to ${ deployBranch } branch` );
+			(error) =>  {
+				if( error ){ reject( error ) }
+				resolve();
 			}
-		);
-	})
-	.catch( error => console.error(error))
+		));
+
+		console.log( `✅ Successfully deployed to GitHub pages: ${ siteDirectory } directory pushed to ${ deployBranch } branch` );
+	} catch(error){
+		console.error( `❌ Failed to deploy to GitHub pages: ${ siteDirectory } directory failed to push to ${ deployBranch } branch\n${ error }` );
+	}
+})();
