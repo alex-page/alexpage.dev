ee44765299dfd1fe2214acbca956102bd42a992f
Alex Page <alex@alexpage.com.au>
2020-04-24 21:39:31 -0400

    Replace fetch-data with data/projects.js w cache

---
+++ src/_data/projects.js
@@ -0,0 +1,59 @@
+const Cache = require("@11ty/eleventy-cache-assets");
+const globby = require('globby');
+const matter = require('gray-matter');
+const fs = require('fs');
+
+const pages = 'src/projects/*/index.md';
+const githubApi = 'https://api.github.com/repos/';
+const npmApi = 'https://api.npmjs.org/downloads/point/last-year/';
+
+
+/**
+ * Read the markdown file and return the value of the key
+ *
+ * @param {string} directory -  The directory of the files
+ * @param {string} frontMatterKey - The key to return the value
+ */
+const getFrontMatter = async (directory, frontMatterKey) => {
+	const projectFiles = await globby([directory]);
+
+	return projectFiles
+		.map(file => fs.readFileSync(file))
+		.map(content => {
+			const {data} = matter(content);
+			return data[frontMatterKey];
+		})
+		.filter(key => key);
+};
+
+
+module.exports = async function() {
+	const githubRepos = await getFrontMatter(pages, 'github');
+	const npmPackages = await getFrontMatter(pages, 'npm');
+
+	// Fetch Github and NPM to get data
+	const repoData = await Promise.all(
+		githubRepos.map(repo =>
+			Cache(`${githubApi}${repo}`, {type: 'json'})
+		)
+	);
+
+	const packageData = await Promise.all(
+		npmPackages.map(npmPackage =>
+			Cache(`${npmApi}${npmPackage}`, {type: 'json'})
+		)
+	);
+
+	const repoStars = repoData.map(({full_name, stargazers_count}) => ({
+		[full_name]: stargazers_count
+	}));
+
+	const packageDownloads = packageData.map(({package, downloads}) => ({
+		[package]: downloads
+	}));
+
+  return {
+		stars: repoStars,
+		downloads: packageDownloads
+  };
+};
