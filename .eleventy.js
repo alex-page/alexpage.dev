4cb343766764e9d4733750348e8be52008266f37
Alex Page <hi@alexpage.dev>
2021-12-12 21:50:16 -0800

    add missing dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,26 @@
 const cleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addNunjucksShortcode(
     "cssBundle",
     () => new cleanCSS({}).minify(["src/_includes/style.css"]).styles
   );
+
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+		if( outputPath && outputPath.endsWith(".html") ) {
+			let minified = htmlmin.minify(content, {
+				minifyCSS: true,
+				minifyJS: true,
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+			});
+			return minified;
+		}
+
+		return content;
+	});
 
   eleventyConfig.addPassthroughCopy("src/images");
 
